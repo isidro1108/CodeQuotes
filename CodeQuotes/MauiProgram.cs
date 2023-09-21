7f216da20d6e1f515d313e5c77b8a2cb3cff1568
ImDev11 <euris110811@gmail.com>
2023-09-21 13:49:06 -0400

    feat: main UI defined

--- CodeQuotes/MauiProgram.cs
+++ CodeQuotes/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace CodeQuotes
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Bison.ttf", "MainFont");
                 });
 
 #if DEBUG
